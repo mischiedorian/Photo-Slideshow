fc8a6655acd6e670b5cd1283f3dc44be59c90b6b
Ciprian-Constantin Hurmuzache <ciprian-constantin.hurmuzache@ing.com>
2019-02-28 21:07:18 +0100

    - compute number of total slides

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,9 +6,13 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import app.Image;
 import java.util.Scanner;
 
 public class Main {
+
+
+
     public static void main(String[] args) throws FileNotFoundException {
         List<Image> images = new ArrayList<>();
 
@@ -18,15 +22,25 @@ public class Main {
 
         int numberOfPictures = Integer.valueOf(scanner.nextLine().split(" ")[0]);
 
+        int nrOfHorizontalPics = 0;
+        int nrOfVerticalPics = 0;
         while (scanner.hasNext()) {
             String line = scanner.nextLine();
             String[] s = line.split(" ");
             String orientation = s[0];
             String numberOfTags = s[1];
             ArrayList<String> currentTags = new ArrayList<>(Arrays.asList(s).subList(2, s.length));
-            images.add(new Image(orientation.toCharArray()[0], Integer.valueOf(numberOfTags), currentTags));
+            char orient = orientation.toCharArray()[0];
+            if(orient == 'H') {
+                nrOfHorizontalPics++;
+            } else {
+                nrOfVerticalPics++;
+            }
+            images.add(new Image(orient, Integer.valueOf(numberOfTags), currentTags));
         }
+        int nrOfTotalSlides = nrOfHorizontalPics + (nrOfVerticalPics)/2;
 
         System.out.println(images);
+        System.out.println(nrOfTotalSlides);
     }
 }
